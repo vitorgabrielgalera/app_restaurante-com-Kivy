d47781d3fdf09ff0ce5b847f9c56abdccb6e8c83
vitorgabrielgalera <vitor.ggabrielgalera@gmail.com>
2024-04-24 20:58:45 -0300

    Tela do garçom e transiçoes no app restaurante

--- app_restaurante.py
+++ app_restaurante.py
@@ -1,16 +1,28 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.config import Config
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
+#defino as configurações da janela
+Config.set('graphics', 'resizable', '0')
+Config.set('graphics', 'width', '360')
+Config.set('graphics', 'heigth', '640')
 
-class TelaPrincipal(GridLayout):
+class TelaPrincipal(Screen):
     pass
 
-class Telagarcom(GridLayout):
+class TelaGarcom(Screen):
     pass
 
-class RestauranteApp(App):
+class GerenciadorTelas(ScreenManager):
+    pass
+
+kv = Builder.load_file('restaurante.kv')
+
+class RstauranteApp(App):
     def build(self):
-        return TelaPrincipal()
+        return kv
     
 if __name__ == "__main__":
-    RestauranteApp().run()
+    RstauranteApp().run()
